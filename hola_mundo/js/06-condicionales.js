774e586f283cb528b01ac00bb212403baf95514e
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-10-31 12:00:14 -0600

    Condicionales y operadores lógicos

--- hola_mundo/js/06-condicionales.js
+++ hola_mundo/js/06-condicionales.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 //Condicional IF
 //Es una estructura de control o instrucciones que permiten comparar algo
@@ -17,19 +17,44 @@ var nombre = "Luis";
     Distinto: !=
 */
 
-if(edad1 >= 18){
-    //Es mayor de edad
-    console.log(nombre + " tiene " + edad1 + " años, es mayor de edad.");
-    if(edad1 <= 33){
-        console.log("Todavia eres milenial");
-    } else if(edad1 >= 70){
-        console.log("Eres anciano.");
-    }
-    else {
-        console.log("Ya no eres milenial");
-    }
+if (edad1 >= 18) {
+  //Es mayor de edad
+  console.log(nombre + " tiene " + edad1 + " años, es mayor de edad.");
+  if (edad1 <= 33) {
+    console.log("Todavia eres milenial");
+  } else if (edad1 >= 70) {
+    console.log("Eres anciano.");
+  } else {
+    console.log("Ya no eres milenial");
+  }
 } else {
-    //Es menor de edad
-    console.log(nombre + " tiene " + edad1 + " años, es menor de edad.");
-    
+  //Es menor de edad
+  console.log(nombre + " tiene " + edad1 + " años, es menor de edad.");
+}
+
+/*
+    OPERADORES LÓGICOS
+    AND(Y): &&
+    OR(O): ||
+    Negación: ! 
+*/
+
+var year = 2018;
+//Negación
+if (year != 2016) {
+    console.log("El año no es 2016, realmente es: " + year);
+}
+
+//AND
+if (year >= 2000 && year <= 2020 && year != 2018) {
+    console.log("Estamos en la era actual");
+} else {
+    console.log("Estamos en la era post moderna");
+}
+
+//OR
+if (year == 2008 || (year >= 2018 && year == 2028)) {
+    console.log("El año aca en 8");
+} else {
+    console.log("Año no registrado");
 }
