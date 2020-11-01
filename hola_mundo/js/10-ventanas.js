977150a7b5bd180196e6d49b6e220476b9240e99
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-01 15:13:12 -0600

    Sección 10: Alertas y ventanas

---
+++ hola_mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//ALERTA
+// alert("Esta es mi alerta!!");
+// alert("Este es mi texto");
+
+//CONFIRMACIÓN
+// var mi_resultado = confirm("¿Estas seguro que quieres continuar?");
+// console.log(mi_resultado);
+
+// INGRESO DATOS
+var mi_resultado = prompt(" ¿Qué edad tienes?", 18);
+console.log(mi_resultado);
