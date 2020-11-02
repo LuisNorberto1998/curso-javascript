a1969c7ec8427122f7c072b71a49db3bf4f8bc36
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-02 14:35:58 -0600

    Ejercicio 5

---
+++ hola_mundo/js/15-ejercicio5.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Programa que muestre todos los números divisores de un número introducido en un prompt
+ */
+
+var numero = parseInt(prompt("Introduce un número:", 1));
+
+for (var i = 1; i <= numero; i++) {
+    if (numero % i == 0) {
+        console.log("Divisor: " + i);
+    }
+}
