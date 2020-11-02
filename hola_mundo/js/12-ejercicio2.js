4619c2888f7ba7e189df76f7d9cdf65c555f871d
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-02 14:14:18 -0600

    Ejercićio 2

---
+++ hola_mundo/js/12-ejercicio2.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//Ejercicio dos
+
+/*
+    Utilizando un bucle, mostrar la suma y la media de los números introducidos
+    hasta introducir un número negativo y mostrar el resultado
+*/
+
+var suma = 0;
+var contador = 0;
+
+do {
+    var numero = parseInt(
+    prompt("Introduce números hasta que metas uno negativo")
+    );
+    if (isNaN(numero)) {
+        numero = 0;
+    } else if (numero >= 0) {
+        suma += numero;
+
+        contador++;
+    }
+
+    console.log(suma);
+    console.log(contador);
+} while (numero >= 0);
+
+alert("La suma de todos los números es: " + suma);
+alert("La media de todos los números es: " + (suma/contador));
