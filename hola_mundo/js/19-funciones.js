2380af9807ccbdb18b16d440eddc375e18006711
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-02 20:17:43 -0600

    43. Parámetros opcionales

--- hola_mundo/js/19-funciones.js
+++ hola_mundo/js/19-funciones.js
@@ -4,19 +4,32 @@
 // Una función es una agrupación reutilizable de un conjunto de instrucciones.
 
 //Defino la función
-function calculadora(numero1, numero2) {
+function calculadora(numero1, numero2, mostrar = false) {
     //Conjunto de instrucciones a ejecutar
-    console.log("Suma: " + (numero1 + numero2));
-    console.log("Resta: " + (numero1 - numero2));
-    console.log("Multiplicación: " + numero1 * numero2);
-    console.log("División: " + numero1 / numero2);
-    console.log("*********************************************");
+    if (mostrar == false) {
+        console.log("Suma: " + (numero1 + numero2));
+        console.log("Resta: " + (numero1 - numero2));
+        console.log("Multiplicación: " + numero1 * numero2);
+        console.log("División: " + numero1 / numero2);
+        console.log(mostrar);
+        console.log("*********************************************");
+    } else {
+        document.write("Suma: " + (numero1 + numero2) + "</br>");
+        document.write("Resta: " + (numero1 - numero2) + "</br>");
+        document.write("Multiplicación: " + numero1 * numero2 + "</br>");
+        document.write("División: " + numero1 / numero2 + "</br>");
+        document.write(mostrar + "</br>");
+        document.write("*********************************************");
+    }
 
     return console.log("Hola soy la calculadora");
 }
 
 //Invocar o llamar a la función
-for (var i = 1; i <= 10; i++) {
-    console.log(i);
-    calculadora(i, 1);
-}
+calculadora(1, 2);
+calculadora(2, 5, true);
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//     calculadora(i, 1);
+// }
