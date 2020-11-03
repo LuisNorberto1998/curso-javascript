fde322148719d0d5721c68aa4f4fd96543c2ee2f
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-02 20:26:59 -0600

    44. Funciones dentro de otras

--- hola_mundo/js/19-funciones.js
+++ hola_mundo/js/19-funciones.js
@@ -4,25 +4,31 @@
 // Una función es una agrupación reutilizable de un conjunto de instrucciones.
 
 //Defino la función
+
+function porConsola(numero1, numero2) {
+    console.log("Suma: " + (numero1 + numero2));
+    console.log("Resta: " + (numero1 - numero2));
+    console.log("Multiplicación: " + numero1 * numero2);
+    console.log("División: " + numero1 / numero2);
+    console.log("*********************************************");
+}
+
+function porPantalla(numero1, numero2) {
+    document.write("Suma: " + (numero1 + numero2) + "</br>");
+    document.write("Resta: " + (numero1 - numero2) + "</br>");
+    document.write("Multiplicación: " + numero1 * numero2 + "</br>");
+    document.write("División: " + numero1 / numero2 + "</br>");
+    document.write("*********************************************");
+}
+
 function calculadora(numero1, numero2, mostrar = false) {
     //Conjunto de instrucciones a ejecutar
     if (mostrar == false) {
-        console.log("Suma: " + (numero1 + numero2));
-        console.log("Resta: " + (numero1 - numero2));
-        console.log("Multiplicación: " + numero1 * numero2);
-        console.log("División: " + numero1 / numero2);
-        console.log(mostrar);
-        console.log("*********************************************");
+        porConsola(numero1, numero2);
     } else {
-        document.write("Suma: " + (numero1 + numero2) + "</br>");
-        document.write("Resta: " + (numero1 - numero2) + "</br>");
-        document.write("Multiplicación: " + numero1 * numero2 + "</br>");
-        document.write("División: " + numero1 / numero2 + "</br>");
-        document.write(mostrar + "</br>");
-        document.write("*********************************************");
+        porPantalla(numero1, numero2);
     }
-
-    return console.log("Hola soy la calculadora");
+    return true;
 }
 
 //Invocar o llamar a la función
