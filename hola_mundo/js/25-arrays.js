d2284877077de74b8da6930b61c00f276954512c
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-04 13:02:19 -0600

    Sección 13: Arrays y Arreglos - Arrays básicos

---
+++ hola_mundo/js/25-arrays.js
@@ -0,0 +1,9 @@
+'use strict'
+
+//Arrays, Arreglos, Matrices
+var nombre = "Luis Norberto";
+var nombres = ["Luis Norberto", "Yoselin Canales", "Datos de prueba", 23, false, true];
+
+var lenguajes = new Array("PHP", "JS", "Python", "Java", "Dart", "C++", "C#");
+
+console.log(nombres[1]);
