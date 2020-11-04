13733e130ace1dbdc03838a0cb7e6b8101641797
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-04 12:29:06 -0600

    49. Métodos para procesar textos

---
+++ hola_mundo/js/23-funciones-texto.js
@@ -0,0 +1,22 @@
+'use stricts'
+
+//Transformación de textos
+var numero = 444;
+var texto1 = "Bienvenido al curso de Javascript";
+var texto2 = "Es muy buen curso :v";
+
+var dato = numero.toString();
+    dato = texto1.toUpperCase();
+    dato = texto2.toUpperCase();
+
+console.log(dato);
+
+//Calcular longitud de un texto
+var nombre = "Luis Norberto";
+    nombre = ["Wenas",  "Wenas"];
+
+console.log(nombre.length);
+
+//Concatenar - Unir textos.
+var textoTotal = texto1.concat(" " + texto2);
+console.log(textoTotal);
