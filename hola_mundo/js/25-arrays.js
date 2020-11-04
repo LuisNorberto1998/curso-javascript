962b8bf8635c14b4afacbb5b8c8b8c31c2276193
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-11-04 13:16:21 -0600

    54. Arrays Avanzados

--- hola_mundo/js/25-arrays.js
+++ hola_mundo/js/25-arrays.js
@@ -1,9 +1,39 @@
-'use strict'
+"use strict";
 
 //Arrays, Arreglos, Matrices
 var nombre = "Luis Norberto";
-var nombres = ["Luis Norberto", "Yoselin Canales", "Datos de prueba", 23, false, true];
+var nombres = [
+    "Luis Norberto",
+    "Yoselin Canales",
+    "Datos de prueba",
+    23,
+    false,
+    true,
+];
 
 var lenguajes = new Array("PHP", "JS", "Python", "Java", "Dart", "C++", "C#");
 
-console.log(nombres[1]);
+// var lenguajes_contar = lenguajes.length;
+// var elemento = parseInt(
+//     prompt(
+//         "Que elemento del número total (0-" +
+//             lenguajes_contar +
+//             ") del array quieres?",
+//         0
+//     )
+// );
+
+// if (elemento >= lenguajes_contar) {
+//     alert("Introduce el número correcto, menor que " + lenguajes_contar);
+// } else {
+//     alert("El usuario seleccionado es: " + nombres[elemento]);
+// }
+
+document.write("<h1>Los lenguajes de programación del 2020</h1>");
+document.write("<ul>");
+
+for (var i = 0; i < lenguajes.length; i++) {
+    document.write("<li>" + lenguajes[i] + "</li>");
+}
+
+document.write("</ul>");
