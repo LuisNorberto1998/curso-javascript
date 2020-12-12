09be0479c6fc8ea75382d6dbe6f42737e279c2ae
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-12-12 12:33:18 -0600

    Operaciones con arrays

--- hola_mundo/js/26-arrays-multi.js
+++ hola_mundo/js/26-arrays-multi.js
@@ -1,9 +1,30 @@
-'use strict'
+"use strict";
 
-var categorias = ['Acción', 'Terror', 'Comedia'];
-var peliculas = ['Iron Man', 'Capitan America', 'Los Vengadores'];
+var categorias = ["Acción", "Terror", "Comedia"];
+var peliculas = ["Iron Man", "Capitan America", "Los Vengadores"];
 
 var cine = [categorias, peliculas];
 
-console.log(cine[0][1]);
-console.log(cine[1][2]);
+// console.log(cine[0][1]);
+// console.log(cine[1][2]);
+
+// var elemento = "";
+
+// do {
+//     elemento = prompt("Introduce tu pelicula");
+//     peliculas.push(elemento);
+// } while (elemento != "ACABAR");
+
+// peliculas.pop();
+
+var indice = peliculas.indexOf('Iron Man');
+
+console.log(indice);
+
+if(indice > -1){
+    peliculas.splice(indice, 1);
+}
+
+var peliculas_array = peliculas.join();
+
+console.log(peliculas_array);
