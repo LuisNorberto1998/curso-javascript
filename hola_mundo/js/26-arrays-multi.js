e354c30cfb5d783ef1c0e10360d97571a6633c13
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-12-12 10:15:33 -0600

    26 - Arrays multidimencionales

---
+++ hola_mundo/js/26-arrays-multi.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var categorias = ['Acción', 'Terror', 'Comedia'];
+var peliculas = ['Iron Man', 'Capitan America', 'Los Vengadores'];
+
+var cine = [categorias, peliculas];
+
+console.log(cine[0][1]);
+console.log(cine[1][2]);
